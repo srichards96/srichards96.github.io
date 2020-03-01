78dc71e5d97e306211e8a79358ebd847d762e8d4
Scott Richards <up777205@myport.ac.uk>
2020-03-01 15:10:14 +0000

    Clicking sidenav link now closes the sidenav.

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
+// Use ES5 for compatability. I'm not gonna set up Babel for something this small...
 (function () {
-  // Get sidenav (on mobile).
   var sidenav = document.querySelector('.nav-links');
   var sidenavToggle = document.querySelector('.landing-header img');
+  var sidenavLinks = document.querySelectorAll('.nav-links a');
 
-  sidenavToggle.addEventListener('click', function (e) {
+  // When toggle is clicked, toggle the sidenav.
+  sidenavToggle.addEventListener('click', function () {
     sidenav.classList.toggle('visible');
   });
+  // When a sidenav link is clicked, close the sidenav if open.
+  (function () { // Contain i
+    for (var i = 0; i < sidenavLinks.length; i++) {
+      sidenavLinks[i].addEventListener('click', function () {
+        sidenav.classList.remove('visible');
+      });
+    }
+  })();
+  
 })();
